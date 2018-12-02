7402fe6ad898b2817a83e65e09c42792170bec53
kidow <wcgo2ling@hanmail.net>
2018-12-02 17:58:21 +0900

    mongoose connect and account => user rename, middlewares set

--- src/app.js
+++ src/app.js
@@ -1,8 +1,30 @@
 require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const port = process.env.PORT || 4000
 
 const app = express()
 
-app.listen(4000, () => {
-  console.log('App listening on port 4000!');
+mongoose.Promise = Promise
+mongoose.connect(process.env.MONGO_URI, { 
+  dbName: 'kidow-bingo', 
+  useNewUrlParser: true, 
+  useCreateIndex: true 
+}, err => {
+  if (err) {
+    console.log('mongodb connection error :', err)
+  } else {
+    console.log('mongodb connected.')
+  }
+})
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+app.listen(port, () => {
+  console.log('App listening on port ' + port);
 });
