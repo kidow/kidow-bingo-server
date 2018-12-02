25e5a2301874ffa2095328c7f2d610bdf2bc1cbf
kidow <wcgo2ling@hanmail.net>
2018-12-02 20:59:56 +0900

    post router 임시 작성

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,5 +1,44 @@
+const User = require('../models/user')
+const Post = require('../models/post')
+const Joi = require('joi')
+const { Types: { ObjectId }} = require('mongoose')
 const express = require('express')
 
 const posts = express.Router()
+
+posts.post('/', async (req, res) => {
+  const { user } = req
+
+  if (!user) {
+    res.status(403)
+    res.json({ message: 'not logged in' })
+    return
+  }
+
+  let account
+  try {
+    account = await User.findById(user._id).exec()
+  } catch (e) {
+    console.error(e)
+    res.status(500)
+  }
+
+  if (!account) {
+    res.sendStatus(403)
+    return
+  }
+
+  const schema = Joi.object().keys({
+
+  })
+
+  const result = Joi.validate(req.body, schema)
+
+  
+})
+
+posts.get('/', async (req, res) => {
+
+})
 
 module.exports = posts
