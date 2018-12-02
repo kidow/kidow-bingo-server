dd4c6cd7e37090edd0f3a09cfa7265053d52f395
kidow <wcgo2ling@hanmail.net>
2018-12-02 19:20:26 +0900

    post methods set

--- src/models/post.js
+++ src/models/post.js
@@ -19,4 +19,18 @@ const Post = new mongoose.Schema({
   timestamps: true
 })
 
+Post.statics.write = function({}) {
+  const post = new this({})
+  return post.save()
+}
+
+Post.statics.list = function({}) {
+  
+}
+
+Post.methods.writeComment = function({username, text}) {
+  this.comments.unshift({username, text})
+  return this.save()
+}
+
 module.exports = mongoose.model('Post', Post)
