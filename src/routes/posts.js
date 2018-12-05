ad1044596c9d41ff7cbf4894f09a4b121123f95a
kidow <wcgo2ling@hanmail.net>
2018-12-05 15:33:46 +0900

    빙고가 등록은 됨. 그러나 하드코딩 심각

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -68,14 +68,7 @@ posts.post('/', async (req, res) => {
 
   let post
   try {
-    post = await Post.write({
-      title, description, oneBingo, twoBingo, threeBingo,
-      cell_1_1, cell_1_2, cell_1_3, cell_1_4, cell_1_5,
-      cell_2_1, cell_2_2, cell_2_3, cell_2_4, cell_2_5,
-      cell_3_1, cell_3_2, cell_3_3, cell_3_4, cell_3_5,
-      cell_4_1, cell_4_2, cell_4_3, cell_4_4, cell_4_5,
-      cell_5_1, cell_5_2, cell_5_3, cell_5_4, cell_5_5
-    })
+    post = await Post.write(req.body)
     await user.increaseCount()
   } catch (e) {
     console.error(e)
